03b08a5f0c3bd416f7ad49ab5da70e2fa8595e8d
erubae <eruba.e@outlook.com>
2023-11-06 13:16:15 -0500

    Update index.js

--- Database/index.js
+++ Database/index.js
@@ -1,8 +1,8 @@
-import courses from "./courses.json" assert { type: 'json' };
-import links from './links.json' assert { type: 'json' };
-import modules from './modules.json' assert { type: 'json' };
-import {courseNav} from './courseNav.js'
-import assignments from "./assignments.json" assert { type: 'json' };;
+import courses from "./courses.json" assert { type: "json" };;
+import links from "./links.json" assert { type: "json" };
+import modules from "./modules.json" assert { type: "json" };
+import {courseNav} from './courseNav.js';
+import assignments from "./assignments.json" assert { type: "json" };
 import { accountNav } from "./accountNav.js";
 export default {
     courses,
