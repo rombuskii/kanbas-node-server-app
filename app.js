1bfbd2de508b881dc3c5aad213854bb8cebd1d85
erubae <eruba.e@outlook.com>
2023-11-23 20:54:22 -0500

    Added sessiom

--- app.js
+++ app.js
@@ -5,14 +5,35 @@ import CourseRoutes from "./courses/routes.js";
 import Lab5 from './Lab5.js'
 import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
+import session from "express-session";
+import UserRoutes from "./users/routes.js";
+import mongoose from "mongoose";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(express.json());
 app.use(cors({
     credentials: true,
-    origin: '*'
+    origin: process.env.FRONTEND_URL,
 }));
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }
+  app.use(session(sessionOptions));
+  
+  
 ModuleRoutes(app);
 CourseRoutes(app);
+UserRoutes(app);
 Lab5(app);
 Hello(app);
 app.listen(process.env.PORT || 4000, () => {
